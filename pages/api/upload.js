04086251bc50d08f909f9b86779079aab5555d2f
JITENDRA KUMAR <jitendrakumar77800@gmail.com>
2025-08-08 17:13:17 +0530

    upload.js

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,21 +1,14 @@
-// pages/api/upload.js
 import { v2 as cloudinary } from "cloudinary";
 import formidable from "formidable";
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+export const config = { api: { bodyParser: false } };
 
-// Cloudinary config
 cloudinary.config({
   cloud_name: "dxbia1pry",
   api_key: "359682453899814",
   api_secret: "C8W4vLxBERmtGNGayv2WFPssZxM",
 });
 
-// Formidable parser function
 const parseForm = (req) => {
   const form = formidable({ multiples: false });
   return new Promise((resolve, reject) => {
@@ -35,19 +28,13 @@ export default async function handler(req, res) {
     const { files } = await parseForm(req);
     const file = files.file;
 
-    if (!file) {
-      return res.status(400).json({ error: "No file uploaded" });
-    }
+    if (!file) return res.status(400).json({ error: "No file uploaded" });
 
-    // Upload to Cloudinary
     const result = await cloudinary.uploader.upload(file.filepath, {
       folder: "nextjs_uploads",
     });
 
-    res.status(200).json({
-      message: "Image uploaded successfully",
-      url: result.secure_url,
-    });
+    res.status(200).json({ message: "Image uploaded successfully", url: result.secure_url });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
